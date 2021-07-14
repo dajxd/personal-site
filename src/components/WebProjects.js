5e21756a736c50a4dd95ea54a31468add058e270
dave <mcnamaracommadavid@gmail.com>
2021-07-14 12:09:52 -0400

    k

--- src/components/WebProjects.js
+++ src/components/WebProjects.js
@@ -20,7 +20,7 @@ export default function WebProjects() {
             <a
               href={project.link}
               key={project.image}
-              className="sm:h-4 w-5 p-4">
+              className="sm:w-1 w-100 p-4">
               <div className="flex relative">
                 <img
                   alt="gallery"
