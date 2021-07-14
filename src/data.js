9c66e4ca26f5c7cdfedae0267dc0d615d84a4dfe
dave <mcnamaracommadavid@gmail.com>
2021-07-14 11:58:17 -0400

    growing growing gone

--- src/data.js
+++ src/data.js
@@ -1,4 +1,72 @@
-export const projects = [
+export const webprojects = [
+  {
+    title: "Dave\'s Student Portal",
+    subtitle: "Node.js, MongoDB, Pug",
+    description:
+      "An always-up-to-date resource for my music students for homework, worksheets, charts, and more.",
+    image: "./studentportal.png",
+    link: "https://www.davesstudents.com",
+  },
+  {
+    title: "Black Duck Moments",
+    subtitle: "Squarespace, Mailchimp, Stripe",
+    description:
+      "A hub for a pain management therapist\'s patients, with a scheduled e-mail support program and book purchase incentives.",
+    image: "./bdmfull.png",
+    link: "",
+  },
+  {
+    title: "FreqFreak Free",
+    subtitle: "Pure javascript audio processing",
+    description:
+      "A not-entirely-serious frequency ear training websites for audio engineers to hone their craft.",
+    image: "./fff.png",
+    link: "http://www.davemcnamara.com/freqfreakfree/",
+  },
+  {
+    title: "Web of Sites",
+    subtitle: "Javascript, shamelessness",
+    description:
+      "A very serious and highly stylized game name generator, inspired by the endless number of mobile games titled like this.",
+    image: "webofsites.png",
+    link: "http://davemcnamara.com/webofsites/",
+  },
+];
+export const deviceprojects = [
+  {
+    title: "React Reserve",
+    subtitle: "MERN Stack",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "./project-1.gif",
+    link: "https://reactbootcamp.com",
+  },
+  {
+    title: "React Tracks",
+    subtitle: "React and Python",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "./project-2.gif",
+    link: "https://reactbootcamp.com",
+  },
+  {
+    title: "DevChat",
+    subtitle: "React and Firebase",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "./project-3.gif",
+    link: "https://reactbootcamp.com",
+  },
+  {
+    title: "Epic Todo App",
+    subtitle: "React Hooks",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "./project-4.gif",
+    link: "https://reactbootcamp.com",
+  },
+];
+export const utilityprojects = [
   {
     title: "React Reserve",
     subtitle: "MERN Stack",
@@ -33,22 +101,22 @@ export const projects = [
   },
 ];
 
-export const testimonials = [
-  {
-    quote:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
-    image: "https://randomuser.me/api/portraits/men/1.jpg",
-    name: "Jesse Hicks",
-    company: "Zoozle",
-  },
-  {
-    quote:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
-    image: "https://randomuser.me/api/portraits/men/94.jpg",
-    name: "Ruben Alvarez",
-    company: "Dooble.io",
-  },
-];
+// export const testimonials = [
+//   {
+//     quote:
+//       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
+//     image: "https://randomuser.me/api/portraits/men/1.jpg",
+//     name: "Jesse Hicks",
+//     company: "Zoozle",
+//   },
+//   {
+//     quote:
+//       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
+//     image: "https://randomuser.me/api/portraits/men/94.jpg",
+//     name: "Ruben Alvarez",
+//     company: "Dooble.io",
+//   },
+// ];
 
 export const skills = [
   "JavaScript",
