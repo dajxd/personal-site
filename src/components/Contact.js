e0128af47ae548c1ecd0c3f4dea8e9a92ccfe950
dave <mcnamaracommadavid@gmail.com>
2021-08-03 16:16:23 -0400

    not a big fan of commits to fix an apostrophe, but here we are!

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -25,7 +25,7 @@ export default function Contact() {
     }
 
     return (
-        <section id="contact" className="relative md:border-none border-b-4">
+        <section id="contact" className="relative">
             <div
                 className="container px-5 py-10 mx-auto flex sm:flex-nowrap flex-wrap resblue rounded-lg overflow-hidden p-10">
                 <div
