0adbc0e1da4ff6fe5f3a05e75a61a461bcafc1f2
dave <mcnamaracommadavid@gmail.com>
2021-08-06 18:41:46 -0400

    why is it so much different on my phone than in firefox's rd mode?

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,10 +3,9 @@ import React from "react";
 export default function Navbar() {
     return (
         <header
-            id="header"
             className="resyellow md:sticky top-0 z-10 md:border-none border-b-4 border-red-300 md:border-none border-b-4 border-red-300">
             <div className="container mx-auto flex flex-wrap md:p-5 p-0 pb-3 flex-col md:flex-row items-center">
-            <div className=" flex items-spread justify-between items-baseline px-5 md:items-center w-full md:w-auto">
+            <div className=" flex items-spread justify-between items-baseline md:px-5 p-0 md:items-center w-full md:w-auto">
                 <p className="title-font-blue font-medium text-white mb-4 md:mb-0">
                     <a href="#top" className="ml-0 md:ml-3 text-xl" id="name">
                         Dave McNamara
